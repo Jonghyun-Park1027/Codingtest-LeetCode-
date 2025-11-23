1d77fd01051a64918becd8767ede5793566a82ae
Jonghyun Park <121370967+Jonghyun-Park1027@users.noreply.github.com>
2025-11-23 14:09:36 +0900

    Time: 3 ms (9.35%), Space: 17.8 MB (37.98%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -1,14 +1,12 @@
 class Solution:
     def numberOfSteps(self, num: int) -> int:
-        count = 0
-        while True :
-            
-            if num <=0 :
-                # count += 1
-                break
-            if num % 2 == 0 :
-                num /= 2
-            else :
-                num -= 1
-            count += 1
-        return count
+        if num == 0:
+            return 0
+        steps = 0
+        while num:
+            if num & 1:  # 맨 끝 비트가 1이면 (홀수)
+                steps += 2
+            else:        # 맨 끝 비트가 0이면 (짝수)
+                steps += 1
+            num >>= 1    # 오른쪽으로 1비트 시프트 (÷2 효과)
+        return steps - 1  # MSB의 과잉 계산 보정
