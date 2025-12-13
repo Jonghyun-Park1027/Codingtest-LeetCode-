43fe2a952c7cd3bb46c25923d3c614149569c2b7
Jonghyun Park <121370967+Jonghyun-Park1027@users.noreply.github.com>
2025-12-13 10:26:14 +0900

    Time: 87 ms (24.17%), Space: 33.4 MB (20.02%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.py
@@ -5,7 +5,7 @@ class Solution:
         ans = 0
         for i in range(1,n):
             prefix.append(prefix[i-1] + nums[i])
-        print(prefix)
+        
         for j in range(n-1):
             if prefix[j] >= prefix[n-1] - prefix[j] :
 
